41c4a7812beffcaefde94d0f1fbe55b50ee38444
Asifiwe BUHENDWA <mickaelbhd@gmail.com>
2025-03-20 16:37:47 +0200

    Adding the interactivity of the navbar button

---
+++ src/scripts/header.js
@@ -0,0 +1,29 @@
+let img;
+const observer = new MutationObserver(() => {
+
+    img = document.getElementById("pepe");
+    img.addEventListener("click", () => {
+        console.log("Click");
+        const source1 =  "/src/assets/close.png"
+        const source2 =  "/src/assets/menu.png"
+        const source = img.getAttribute("src");
+        if (source == source1) {
+            img.setAttribute("src", source2);
+            document.getElementById("navbar").classList.add("hide");
+        }else{
+            img.setAttribute("src", source1);
+            document.getElementById("navbar").classList.remove("hide");
+
+        }
+        
+        stopObserver();
+    })
+    
+
+})
+
+observer.observe(document.getElementById("header"), { childList: true, subtree: true })
+
+
+const stopObserver = ()=> observer.disconnect()
+
